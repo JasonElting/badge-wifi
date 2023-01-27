8815314f5e87cacd05585a814926d50d8399f87b
Jason Elting <jason@elting.dev>
2023-01-26 20:37:44 -0500

    start

--- src/main.cpp
+++ src/main.cpp
@@ -14,25 +14,27 @@ void setup()
  
 void loop()
 {
-    Serial.println("scan start");
+    // Serial.println("scan start");
  
     // WiFi.scanNetworks will return the number of networks found
     int n = WiFi.scanNetworks();
-    Serial.println("scan done");
+    // Serial.println("scan done");
     if (n == 0) {
         Serial.println("no networks found");
     } else {
-        Serial.printf("%2s %-32s %-17s %s\n",
-            "#", "SSID", "BSSID", "RSSI");
+        // Serial.printf("%2s %-32s %-17s %s\n",
+        //     "#", "SSID", "BSSID", "RSSI");
         for (int i = 0; i < n; ++i) {
+          // if(WiFi.BSSIDstr(i)=="90:6C:AC:F9:87:D0"){
             // Print SSID and RSSI for each network found
             Serial.printf("%2u %-32s %s %d\n",
                 i, WiFi.SSID(i).c_str(), WiFi.BSSIDstr(i).c_str(), WiFi.RSSI(i));
-            delay(10);
+            delay(0.2);
+          // }
         }
     }
     Serial.println("");
  
     // Wait a bit before scanning again
-    delay(5000);
+    delay(500);
 }
